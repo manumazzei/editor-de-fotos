b55fbbfd1a259a4e686b350fabf7cc82b1645b18
Karolina <karolinaborgesk@gmail.com>
2023-07-10 15:47:02 -0300

    WIP: configuração de cadastro

--- src/modules/auth/views/Register.vue
+++ src/modules/auth/views/Register.vue
@@ -11,7 +11,23 @@ const router = useRouter();
 const email = ref("");
 const password = ref("");
 const passwordtest = "";
+
+
 const show = false;
+const required = true;
+const form = false;
+
+function passwordRules(input) {
+  let regexSpecial = /\W|_/;
+  let regexNumber = /(\d+)| /g;
+  if (input.includes(" ")) return "No spaces";
+  if ((input.length < 8, !regexSpecial.test(input), !regexNumber.test(input)))
+    return "At least 8 characters, 1 special character and 1 number";
+  return true;
+}
+function confirmPassword(passwordtest) {
+  return passwordtest !== this.password ? "Passwords don't match" : true;
+}
 
 async function handleSignUp() {
   const result = await auth.signUp(email.value, password.value);
@@ -21,50 +37,42 @@ async function handleSignUp() {
   }
   router.push("/login");
 }
-
-function requiredRules (input) {
-      return !!input || "Field is required";
-    }
-
-function passwordRules(input) {
-      let regexSpecial = /\W|_/;
-      let regexNumber = /(\d+)| /g;
-      if (input.includes(" ")) return "No spaces";
-      if (input.length < 8, !regexSpecial.test(input), !regexNumber.test(input))
-        return "At least 8 characters, 1 special character and 1 number";
-      return true;
-    }
-function confirmPassword(passwordtest) {
-      return passwordtest !== this.password ? "Passwords don't match" : true;
-    }
-
-
 </script>
 
 <template>
   <h1>REGISTER</h1>
   <main>
-    <input type="email" v-model="email" />
+    <form>
 
-    <input 
-    v-model="password"
-    :rules="[requiredRules, passwordRules]"
-    label="Password"
-    :type="show ? 'text' : 'password'"
-    :append-inner-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-    @click:append-inner="show = !show"
-    />
+      <input 
+      required 
+      type="email" 
+      v-model="email" 
+      placeholder="Email" />
 
-    <input 
-    v-model="passwordtest"
-    :rules="[requiredRules, confirmPassword]"
-    label="Confirm Password"
-    :type="show ? 'text' : 'password'"
-    :append-inner-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-    @click:append-inner="show = !show"/>
+      <input
+        v-model="password"
+        required
+        placeholder="Password"
+      />
+      <!-- :type="show ? 'text' : 'password'"
+        :append-inner-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+        @click:append-inner="show = !show" /> -->
 
-    <button @click="handleSignUp">Sign Up</button>
+      <input
+        v-model="passwordtest"
+        required
+        placeholder="Confirm Password"
+      />
+      <!-- :type="show ? 'text' : 'password'"
+          :append-inner-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append-inner="show = !show"/> -->
+      </form>
 
+    <button 
+    @click="handleSignUp">
+    Sign Up
+  </button>
   </main>
 </template>
 
@@ -72,6 +80,6 @@ function confirmPassword(passwordtest) {
 main {
   display: flex;
   flex-direction: column;
-  gap: 1rem ;
+  gap: 1rem;
 }
 </style>
