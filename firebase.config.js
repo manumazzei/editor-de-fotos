9aed3cfb4ff2c32916e68bac1e509fe41d0f6b6e
Emmanuelle <emmanuellematos9@gmail.com>
2023-07-11 11:18:55 -0300

    configurações da chamada do firebase.

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +16,11 @@ const firebaseConfig = {
   storageBucket: "final-project-632a9.appspot.com",
   messagingSenderId: "748855832503",
   appId: "1:748855832503:web:465c660edc6012e13fc232",
-  measurementId: "G-2NCZCP1KKH"
+  measurementId: "G-2NCZCP1KKH",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
+export const storage = getStorage(app);
