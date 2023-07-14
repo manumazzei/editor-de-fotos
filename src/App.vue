613c5494e5518da329c62ff20f0b75e2baa62d5e
Karolina <karolinaborgesk@gmail.com>
2023-07-14 15:30:45 -0300

    bloqueia acesso de quem não esta logado

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <script setup>
+import { RouterView } from "vue-router";
+// import { onMounted } from "vue";
+// import { useStore } from "./composables/useStore";
 
-import { RouterLink, RouterView } from 'vue-router'
-import { onMounted } from "vue";
-import { useStore } from "./composables/useStore";
+// const { content } = useStore();
+// const { auth } = content;
 
-const { content } = useStore();
-const { auth } = content;
-onMounted(() => {
-  const user = JSON.parse(
-    localStorage.getItem(
-      "firebase:authUser:0amaGqvizzUGksrJd323rpK9Wqt2:[DEFAULT]"
-      
-    )
-  );
+// onMounted(() => {
+//   const user = JSON.parse(
+//     localStorage.getItem(
+//       "firebase:authUser:0amaGqvizzUGksrJd323rpK9Wqt2:[DEFAULT]"
+//     )
+//   );
 
-  if (user) {
-    auth.setUser(user);
-  }
-});
+//   if (user) {
+//     auth.setUser(user);
+//   }
+// });
 </script>
 
 <template>
   <RouterView />
 </template>
 
-<style scoped>
-
-</style>
-
-
+<style scoped></style>
