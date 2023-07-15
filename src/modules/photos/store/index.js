5c8eeb0771494676d91a0982266b0e91bdd6763c
Emmanuelle <emmanuellematos9@gmail.com>
2023-07-15 01:31:03 -0300

    retira erros no store.

--- src/modules/photos/store/index.js
+++ src/modules/photos/store/index.js
@@ -9,37 +9,4 @@ export const photoStore = reactive({
     const storageRef = ref(storage, name);
     return await Save(storageRef, file);
   },
-  applyFilters() {
-    if (this.image) {
-      const brightness = this.brightness / 100; // Escala de -1 a 1
-      const contrast = (this.contrast + 100) / 100; // Escala de 0 a 2
-
-      const brightnessFilter = new fabric.Image.filters.Brightness({
-        brightness: brightness,
-      });
-      const contrastFilter = new fabric.Image.filters.Contrast({
-        contrast: contrast,
-      });
-
-      this.image.filters = [brightnessFilter, contrastFilter];
-      this.image.applyFilters();
-      this.canvas.requestRenderAll();
-    }
-  },
 });
-
-export default {
-  data() {
-    return {
-      showCropArea: false,
-      cropArea: null,
-      selectArea: false,
-      newWidth: 0,
-      newHeight: 0,
-      selectedFilter: false,
-    };
-  },
-  methods: {
-    
-  },
-};
